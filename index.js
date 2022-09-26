2af0b3f422ddbba71fcfce2f1ac7ad39e1474f25
kamel Barakat <kamelbarakat96@yahoo.com>
2022-09-26 19:05:58 -0400

    Bonus Complete

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//bonus
+
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
 /*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
@@ -14,13 +30,13 @@ document.body.append(pineTree) */
 
 
 function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
+    let item = document.createElement('img')
+    item.src = url
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+    return item
 }
 
 newImage('assets/green-character.gif' ,100, 100)
@@ -33,9 +49,9 @@ newImage('assets/well.png', 500, 420)
 
 /*let sword = document.createElement('img')
 sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
+sword.style.position = 'fixed'
+sword.style.left = '500px'
+sword.style.bottom = '405px'
 document.body.append(sword) 
 
 sword.addEventListener('click', function(){
@@ -43,13 +59,15 @@ sword.addEventListener('click', function(){
 }) */
 
 function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
+    let item = newImage(url, left, bottom)
 
-    object.addEventListener('dblclick', () => {
-        object.remove()
+    item.addEventListener('dblclick', () => {
+        item.remove()
     })
 }
 
 newItem('assets/sword.png', 500, 405)
-newItem('assets/sheild.png', 165, 185)
+newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
+
